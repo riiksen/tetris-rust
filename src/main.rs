107d80b8abc47900f3814eae7cf67a7ae8075ce2
Mike <certexplay@gmail.com>
2019-03-19 16:36:15 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+extern crate piston;
+extern crate opengl_graphics;
+extern crate graphics;
+extern crate touch_visualizer;
+
+#[cfg(feature = "include_sdl2")]
+extern crate sdl2_window;
+#[cfg(feature = "include_glfw")]
+extern crate glfw_window;
+
+use touch_visualizer::TouchVisualizer;
+use opengl_graphics::{ GLGraphics, OpenGL };
+use graphics::{ Context, Graphics };
+use std::collections::HashMap;
+use piston::window::*;
+
+fn main() {
+    let mut window: PistonWindow =
+        WindowSettings::new("tetris", [480, 640])
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    while let Some(event) = window.next() {
+
+    }
+
+
+
+
+    println!("Hello, world!");
+}
