de75d140fae1dee81acaa7079177970a1822bce0
Mike <certexplay@gmail.com>
2019-03-19 16:47:45 +0100

    added deps

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ extern crate touch_visualizer;
 extern crate sdl2_window;
 #[cfg(feature = "include_glfw")]
 extern crate glfw_window;
+#[cfg(feature = "include_glutin")]
+extern crate glutin_window;
 
 use touch_visualizer::TouchVisualizer;
 use opengl_graphics::{ GLGraphics, OpenGL };
@@ -14,8 +16,15 @@ use graphics::{ Context, Graphics };
 use std::collections::HashMap;
 use piston::window::*;
 
+#[cfg(feature = "include_sdl2")]
+use sdl2_window::Sdl2Window as AppWindow;
+#[cfg(feature = "include_glfw")]
+use glfw_window::GlfwWindow as AppWindow;
+#[cfg(feature = "include_glutin")]
+use glutin_window::GlutinWindow as AppWindow;
+
 fn main() {
-    let mut window: PistonWindow =
+    let mut window: AppWindow =
         WindowSettings::new("tetris", [480, 640])
         .exit_on_esc(true)
         .build()
